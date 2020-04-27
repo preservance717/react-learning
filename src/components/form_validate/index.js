24f0f5b671ad1de6edd9c43c00d8ac3778d38633
LG <gl717@outlook.com>
2020-04-27 15:06:18 +0800

    init project

---
+++ src/components/form_validate/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './index.scss';
+
+class FormValidate extends Component {
+  render() {
+    return (
+      <div>
+        FormValidate
+      </div>
+    );
+  }
+}
+
+export default FormValidate;
+
